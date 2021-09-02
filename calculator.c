168b0c6e033a7e8a733affcf295821a3603743bd
Sudiptamaity123 <80695244+Sudiptamaity123@users.noreply.github.com>
2021-09-02 09:42:14 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+void main(void){
+    printf("....::Welcome To Calculator::....\n\n\n");
+    printf("This is lower version of calculator,basic operations will avilable in this,no scientific calculation won't be avilable, please wait for the stable version....\n\n\n");
+    printf("..............Let's calculate some calculation..............\n\n");
+    int num1,num2;
+    char sign;
+    start:
+    scanf("%d %s %d",&num1,&sign,&num2);
+    switch(sign){
+        case '+':
+        printf("%d + %d = %d\n\n\n",num1,num2,num1+num2);
+        break;
+        case '-':
+        printf("%d - %d = %d\n\n\n",num1,num2,num1-num2);
+        break;
+        case '*':
+        printf("%d * %d = %d\n\n\n",num1,num2,num1*num2);
+        break;
+        case '/':
+        printf("%d / %d = %d\n\n\n",num1,num2,num1/num2);
+        break;
+        case '%':
+        printf("%d '%' %d = %d\n\n\n",num1,num2,num1%num2);
+        break;
+        case '^':
+        printf("%d ^ %d = %d\n\n\n",num1,num2,(int)pow(num1,num2));
+        break;
+        case '&':
+        printf("%d & %d = %d\n\n\n",num1,num2,num1&num2);
+        break;
+        case '|':
+        printf("%d | %d = %d\n\n\n",num1,num2,num1|num2);
+        break;
+        
+
+
+        default:
+        printf("Calculator is under maintanance... This operation will avilable soon.......\n");
+    }
+    goto start;
+}
